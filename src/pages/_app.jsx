287884e04590a0f50a2af6059d4b586762b35ddc
Marcelo Sepulveda <marcelo.sepulveda.ramirez@gmail.com>
2025-06-08 01:33:00 -0400

    Reactivar Header component for SSR test

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,6 @@
 // src/pages/_app.jsx
 
-import '../styles/globals.css';
+import '../styles/globals.css';  // tu CSS con Tailwind, Merriweather, fondo, etc.
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,17 @@
 // src/pages/index.jsx
 
+import Header from '../components/Header';
+
 export default function Home() {
   return (
-    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
-      🚧 Sitio en construcción 🚧
-    </div>
+    <>
+      {/* Header fijo en top */}
+      <Header />
+
+      {/* Mensaje temporal de construcción */}
+      <main style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+        🚧 Sitio en construcción 🚧
+      </main>
+    </>
   );
 }
