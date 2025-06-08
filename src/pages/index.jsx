8fe588a5f39bc38f3064bd6c9e5992e6fbe22fea
Marcelo Sepulveda <marcelo.sepulveda.ramirez@gmail.com>
2025-06-07 22:28:30 -0400

    isolating more

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,6 @@
 // src/pages/index.jsx
 
-import { SEO } from '../utils/seo';
+//import { SEO } from '../utils/seo';
 import Header from '../components/Header';
 import Hero from '../components/Hero';
 // import dynamic from 'next/dynamic';
@@ -16,7 +16,7 @@ export default function Home() {
   return (
     <>
       {/* Configuración SEO */}
-      <SEO
+      {/* <SEO
         title="Piscinas Patricio Luengo - Instalación de Piscinas en Chile"
         description="Especialistas en construcción de piscinas a medida. Confianza, experiencia y diseño personalizado."
         keywords={[
@@ -27,7 +27,7 @@ export default function Home() {
           'construcción de piscinas'
         ]}
         url="https://www.piscinaspatricioluengo.cl/"
-      />
+      /> */}
 
       {/* Header fijo en top */}
       <Header />
