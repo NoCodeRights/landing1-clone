c65e16f918bdf262e4706bc6499e2695561734c5
Marcelo Sepulveda <marcelo.sepulveda.ramirez@gmail.com>
2025-06-07 22:14:59 -0400

    isolating SSR error

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,20 +3,19 @@
 import { SEO } from '../utils/seo';
 import Header from '../components/Header';
 import Hero from '../components/Hero';
-import dynamic from 'next/dynamic';
-// Carrusel client-only
-const GalleryCarousel = dynamic(
-  () => import('../components/GalleryCarousel'),
-  { ssr: false }
-);
-import Process from '../components/Process';
-// import Testimonials from '../components/Testimonials'; // comentado
-import FAQ from '../components/FAQ';
-import Footer from '../components/Footer';
+// import dynamic from 'next/dynamic';
+// const GalleryCarousel = dynamic(
+//   () => import('../components/GalleryCarousel'),
+//   { ssr: false }
+// );
+// import Process from '../components/Process';
+// import FAQ from '../components/FAQ';
+// import Footer from '../components/Footer';
 
 export default function Home() {
   return (
     <>
+      {/* Configuración SEO */}
       <SEO
         title="Piscinas Patricio Luengo - Instalación de Piscinas en Chile"
         description="Especialistas en construcción de piscinas a medida. Confianza, experiencia y diseño personalizado."
@@ -30,17 +29,22 @@ export default function Home() {
         url="https://www.piscinaspatricioluengo.cl/"
       />
 
+      {/* Header fijo en top */}
       <Header />
 
+      {/* Secciones de la página */}
       <main>
         <Hero />
+        {/*
         <GalleryCarousel />
         <Process />
-        {/* <Testimonials /> */}
         <FAQ />
+        */}
       </main>
 
+      {/*
       <Footer />
+      */}
     </>
   );
 }
